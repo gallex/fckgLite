fdf951b8b8b6ffe6626be043147a499099c664e5
gallex <gallex2003@163.com>
2013-11-03 01:53:49 +0800

    add chinese local

---
+++ action/lang/zh.php
@@ -0,0 +1,28 @@
+<?php
+global $fckg_lang;
+
+$fckg_lang['btn_dw_edit'] = "DW编辑";
+$fckg_lang['dw_btn_fck_preview']="FCK预览";
+$fckg_lang['dw_btn_lang']="语言";
+$fckg_lang['title_dw_delete'] = "删除页面";
+$fckg_lang['title_dw_edit'] = "存档，退出，并切换到文本编辑器";
+$fckg_lang['dw_btn_revert'] = "还原";
+$fckg_lang['title_dw_revert'] = "还原备份档";
+$fckg_lang['title_dw_lang']="选择拼写检查的语言";
+$fckg_lang['title_dw_cancel']='退出编辑器';
+$fckg_lang['btn_fck_edit'] = "所见即所得编辑器";
+$fckg_lang['confirm_delete'] = "确定要删除页面吗?";
+$fckg_lang['confirm_preview'] = "你将丢失尚未存储的资料";
+
+$fckg_lang['dw_btn_backup'] ="备份";
+$fckg_lang['title_dw_backup'] ="备份现有资料，并更新锁定计时器";
+$fckg_lang['backup_empty'] = "暂存区没有资料可还原，仍要继续吗?";
+$fckg_lang['btn_draft'] = "载入草稿";
+$fckg_lang['title_draft'] = "查看并编辑草稿";
+$fckg_lang['btn_exit_draft'] = "编辑草稿";
+$fckg_lang['title_draft'] = "查看并编辑草稿";
+$fckg_lang['title_exit_draft'] = "返回当前的文本";
+$fckg_lang['draft_msg']= "有份编辑了一半的草稿。请用下方的草稿按钮继续编辑或删除" ; 
+$fckg_lang['minor_changes'] = "细小修改";
+$fckg_lang['complex_tables']= "复杂表格";
+
